a34596b1158089da6a4730cd708432d6e62e8340
hyojin425 <gywls5184@gmail.com>
2025-03-05 13:57:44 +0900

    init

---
+++ src/main/java/com/example/jobadreambackend/JobadreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobadreambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobadreamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobadreamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobadreambackend/JobadreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobadreambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobadreamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
